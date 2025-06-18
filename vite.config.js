1854c99cfb4c09d5adb094928d20e9a01fdb057d
Musyaffaa2 <musyaffaarwin@gmail.com>
2025-06-18 15:21:31 +0700

    update: add GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/proyek-awal-web-intermediate/',
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'), 
   build: {
